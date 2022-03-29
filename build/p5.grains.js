7b150517578547a16e76aa003ef1f783e06b4f16
itsKaspar <kaspar.ravel@gmail.com>
2022-03-29 11:35:24 -0700

    fgfhfdg

--- build/p5.grains.js
+++ build/p5.grains.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 function grainCircle(cx,cy,cr,c= color(0), dir=TWO_PI){
 
 	stroke(c);
@@ -64,3 +65,5 @@ function grainTriangle(x0, y0, x1, y1, x2, y2){
  // functions : integral
  // find solutions
  // intersect with other objects
+
+},{}]},{},[1]);
